62c0dcdabc119b35c7e913289cb2ba71fbaf8d4e
Death-Prince <nagaljohnllenard@gmail.com>
2024-04-15 08:46:31 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var siteData = [
+    {
+        'category_name': 'AI',
+        'site_link': 'https://chat.openai.com/',
+        'site_image': 'Chatgpt.png',
+        'site_name': 'ChatGPT',
+    },
+    {
+        'category_name': 'AI',
+        'site_link': 'https://gemini.google.com/app',
+        'site_image': 'gemini.png',
+        'site_name': 'Gemini',
+    },
+    {
+        'category_name': 'Icons',
+        'site_link': 'https://icons8.com/',
+        'site_image': 'Icons8.png',
+        'site_name': 'Icons8',
+    }
+];
