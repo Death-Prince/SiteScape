4858818a1d7e17f548a02b99e2633e1e6c273d36
Death-Prince <nagaljohnllenard@gmail.com>
2024-04-16 10:32:39 +0800

    second commit

--- script.js
+++ script.js
@@ -16,5 +16,35 @@ var siteData = [
         'site_link': 'https://icons8.com/',
         'site_image': 'Icons8.png',
         'site_name': 'Icons8',
+    },
+    {
+        'category_name': 'UI',
+        'site_link': '',
+        'site_image': 'UIverse.png',
+        'site_name': 'UIverse',
+    },
+    {
+        'category_name': 'UI',
+        'site_link': '',
+        'site_image': 'ShadcnUI.png',
+        'site_name': 'Shadcn UI',
+    },
+    {
+        'category_name': 'Tools',
+        'site_link': '',
+        'site_image': '',
+        'site_name': 'LightPDF',
+    },
+    {
+        'category_name': 'AI',
+        'site_link': '',
+        'site_image': '',
+        'site_name': 'TinyWow',
+    },
+    {
+        'category_name': 'Tools',
+        'site_link': '',
+        'site_image': '',
+        'site_name': 'Fake Details',
     }
 ];
