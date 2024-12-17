a3ea802cd8bcd35585b1f6b5269a77e1940ffc30
Death-Prince <nagaljohnllenard@gmail.com>
2024-12-18 04:29:31 +0800

    data

--- textarea.js
+++ textarea.js
@@ -1,10 +1,44 @@
 // textarea.js
 var siteData = [
     {
-        content: `Hello,
+        content: `
+sudo apt install nginx
 
-sudo apt update
-sudo apt upgrade
-sudo systemctl start service`
-    }
+sudo systemctl status nginx
+
+sudo systemctl start nginx
+
+sudo systemctl stop nginx
+
+sudo systemctl restart nginx
+
+sudo systemctl reload nginx
+
+sudo systemctl enable nginx
+
+sudo systemctl disable nginx
+
+sudo systemctl list-dependencies nginx
+
+sudo systemctl is-active nginx
+
+sudo systemctl is-failed nginx
+
+sudo systemctl is-enabled nginx
+
+ TASK B
+
+systemctl list-units --type=service --all
+
+systemctl list-unit-files
+
+systemctl list-unit-files --type=service | grep enabled
+
+systemctl list-unit-files --type=service | grep static
+
+systemctl list-unit-files --type=service | grep disabled
+
+systemctl list-units --type=service --state=active
+
+`}
 ];
