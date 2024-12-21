ca9f5e89ac965fa6abfd79592d8e74fb9e793ca4
Death-Prince <nagaljohnllenard@gmail.com>
2024-12-21 14:13:34 +0800

    exam

--- exam.js
+++ exam.js
@@ -2,8 +2,8 @@
 var siteData = [
     {
         content: `
-CREATE TO USERS ADMIN AND USERS ADJUST NETWORKS ADAPTER 1 TO NAT AND ADAPTER 2 TO HOST-ONLY ADAPTER
-ADJUST THE ETHERNET BOTH ADMIN AND USER IN ADAPTER 2 
+CREATE TO USERS ADMIN AND CLIENT ADJUST NETWORKS ADAPTER 1 TO NAT AND ADAPTER 2 TO HOST-ONLY ADAPTER
+ADJUST THE ETHERNET BOTH ADMIN AND CLIENT IN ADAPTER 2 
  
 ADMIN
  - Choose IPV4
@@ -11,23 +11,32 @@ ADMIN
  - IPv4 Address: 192.168.3.10
  - Subnet Mask: 255.255.255.0
  - Default Gateway: 192.168.3.1
+CLIENT
+ - Choose IPV4
+ - Manual
+ - IPv4 Address: 192.168.3.20
+ - Subnet Mask: 255.255.255.0
+ - Default Gateway: 192.168.3.1
 
-TERMINAL
-sudo nano /etc/netplan/00-installer-config.yaml
+PING ADMIN FROM CLIENT
+ping 192.168.3.10
+PING CLIENT FROM ADMIN
+ping 192.168.3.20
 
-network:
-  ethernets:
-    enp0s8:
-      dhcp4: no
-      addresses: [192.168.3.10/24]  # Or [192.168.3.11/24] depending on the machine
-  version: 2
+ADMIN
+sudo groupadd --gid 1001 "WebDev Team"
+sudo useradd --uid 1500 --gid 1001 --create-home devuser
+sudo usermod -aG "WebDev Team" devuser
+sudo usermod -aG "WebDev Team" $(whoami)
+getent group "WebDev Team"
 
-  - CTRL 0
-  - ENTER
-  - CTRL X
+CLEINT
+sudo groupadd --gid 1001 "WebDev Team"
+sudo useradd --uid 1500 --gid 1001 --create-home devuser
+sudo usermod -aG "WebDev Team" devuser
+sudo usermod -aG "WebDev Team" $(whoami)
+getent group "WebDev Team"
 
-sudo netplan apply
-ip a
 
 
 `}
