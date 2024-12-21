ba5215c72a3e18b291d546e56fb5cf3318e839a3
Death-Prince <nagaljohnllenard@gmail.com>
2024-12-21 11:38:27 +0800

    done

--- textarea.js
+++ textarea.js
@@ -10,26 +10,14 @@ sudo groupadd Finance
 sudo groupadd Manpower
 
 2. creating users
-sudo useradd -G Finance fin_user1
-sudo useradd -G Finance fin_user2
-sudo useradd -G Finance fin_user3
-sudo useradd -G Manpower man_user4
-sudo useradd -G Manpower man_user5
-sudo useradd -G Manpower man_user6
-
-removing
-
-sudo userdel -r fin_user1
-sudo userdel -r fin_user2
-sudo userdel -r fin_user3
-sudo userdel -r man_user4
-sudo userdel -r man_user5
-sudo userdel -r man_user6
-
+sudo useradd -g Finance -G Finance fin_user1
+sudo useradd -g Finance -G Finance fin_user2
+sudo useradd -g Finance -G Finance fin_user3
+sudo useradd -g Manpower -G Manpower man_user4
+sudo useradd -g Manpower -G Manpower man_user5
+sudo useradd -g Manpower -G Manpower man_user6
 
 cat /etc/group
-
-
 
 3.assigning users to group(s)
 gpasswd -A fin_user1 Finance
@@ -37,9 +25,41 @@ gpasswd -A man_user4 Manpower
 
 
 4.setting a user as group administrator
+mkdir /home/fin_user1
+mkdir /home/fin_user1/innovation
+sudo chown fin_user1:Finance /home/fin_user1/innovation
+ls -l /home/fin_user1
+
+4.1. Creating file named Splash inside innovation directory. 
+sudo touch /home/fin_user1/innovation/splash
+sudo chgrp -R Finance /home/fin_user1/innovation
+ls -l /home/fin_user1/innovation
+
+4.2 setting a user as group administrator
+mkdir /home/man_user4
+mkdir /home/man_user4/innovation
+sudo chown man_user4:Manpower /home/man_user4/innovation
+ls -l /home/man_user4
+
+4.3. Creating file named Splash inside innovation directory. 
+sudo touch /home/man_user4/innovation/splash
+sudo chgrp -R Manpower /home/man_user4/innovation
+ls -l /home/man_user4/innovation
 
 5.setting file and directory ownership
+sudo chown -R :Finance /home/fin_user1/innovation
+sudo chown -R :Manpower /home/man_user4/innovation
+
 6.setting file and directory modes
+sudo chmod -R 770 /home/fin_user1/innovation
+sudo chmod -R 770 /home/man_user4/innovation
+
+
+TESTING
+Verify Group Members:
+getent group Finance
+getent group Manpower
 
 `}
 ];
+
